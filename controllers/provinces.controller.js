f25e12aaa6fe1a7c1b53fe174af566c5b2d34913
lehuuhieu-0805 <01643815296h@gmail.com>
2022-09-14 22:31:02 +0700

    update api findall: join 3 table

--- controllers/provinces.controller.js
+++ controllers/provinces.controller.js
@@ -1,10 +1,11 @@
 const connection = require('../config/db.config')
 const Districts = require('../models/districts.model')
 const Provinces = require('../models/provinces.model')
+const Wards = require('../models/wards.model')
 
 const findAll = async () => {
   try {
-    const provinces = await Provinces.findAll({ include: Districts })
+    const provinces = await Provinces.findAll({ include: [{ model: Districts, include: [{ model: Wards }] }] })
     connection.close()
     return provinces
   } catch (err) {
